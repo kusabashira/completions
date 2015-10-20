f940613c42d02eaa69bc894517efb9c9af3752d4
kusabashira <kusabashira227@gmail.com>
2015-10-20 17:18:51 +0900

    Add fish-completion for alita

---
+++ fish/alita.fish
@@ -0,0 +1,7 @@
+complete -c alita -s d -l delimiter -d 'delimit line by DELIM'
+complete -c alita -s r -l regexp -d 'DELIM is a regular expression'
+complete -c alita -s c -l count -d 'delimit line COUNT times'
+complete -c alita -s m -l margin -d 'join cells by FORMAT'
+complete -c alita -s j -l justify -d 'justify cells by SEQUENCE'
+complete -c alita -s h -l help -d 'show this help message'
+complete -c alita -l version -d 'print the version'
