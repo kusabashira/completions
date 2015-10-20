36ea3255f319d381db22e5604f612a43b8144e5a
kusabashira <kusabashira227@gmail.com>
2015-10-20 14:26:27 +0900

    Add fish-completion for gttrans

---
+++ fish/gttrans.fish
@@ -0,0 +1,3 @@
+complete -c gttrans -s h -l help -d 'show this help message'
+complete -c gttrans -s g -l get -d 'show the current transparency'
+complete -c gttrans -s s -l set -d 'set the transparency'
