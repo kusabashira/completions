06944fe8c67ac07d64a590fbba1f09e9fb3811d8
kusabashira <kusabashira227@gmail.com>
2015-10-25 19:17:52 +0900

    Add bash-completion for csvp

---
+++ bash/csvp.bash
@@ -0,0 +1,21 @@
+_csvp()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  local opts="
+    --indexes=
+    --headers=
+    --delimiter=
+    --help
+    --version
+  "
+  case "$cur" in
+    -*)
+      COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+      ;;
+    *)
+      _filedir
+      ;;
+  esac
+  [[ ${COMPREPLY[0]} == *= ]] && compopt -o nospace
+}
+complete -F _csvp csvp
