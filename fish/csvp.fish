732f8ed47234163a587214fc6fc080f611043244
kusabashira <kusabashira227@gmail.com>
2015-10-25 19:27:12 +0900

    Add fish-completion for csvp

---
+++ fish/csvp.fish
@@ -0,0 +1,5 @@
+complete -c csvp -s i -l indexes -d 'select only these indexes'
+complete -c csvp -s h -l headers -d 'select only these headers'
+complete -c csvp -s d -l delimiter -d 'use STRING as the output delimiter (default: \t)'
+complete -c csvp -l help -d 'display this help text and exit'
+complete -c csvp -l version -d 'display version information and exit'
