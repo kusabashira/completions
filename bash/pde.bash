24479bf112826be21c4d42ccd1a7d35a8378e756
kusabashira <kusabashira227@gmail.com>
2015-11-26 13:06:27 +0900

    Add completions for pde

---
+++ bash/pde.bash
@@ -0,0 +1,18 @@
+_pde()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  local opts="
+    --help
+    --version
+  "
+  case "$cur" in
+    -*)
+      COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+      ;;
+    *)
+      _filedir
+      ;;
+  esac
+  [[ ${COMPREPLY[0]} == *= ]] && compopt -o nospace
+}
+complete -F _pde pde
---
+++ fish/pde.fish
@@ -0,0 +1,2 @@
+complete -c pde -s h -l help -d 'display this help text and exit'
+complete -c pde -s v -l version -d 'output version information and exit'
