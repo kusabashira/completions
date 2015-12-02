ed17b5208b203366017d95d5a32c743344bb1655
kusabashira <kusabashira227@gmail.com>
2015-12-03 00:17:41 +0900

    Add completions for rl

---
+++ bash/rl.bash
@@ -0,0 +1,19 @@
+_rl()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  local opts="
+    --delimiter=
+    --help
+    --version
+  "
+  case "$cur" in
+    -*)
+      COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+      ;;
+    *)
+      _filedir
+      ;;
+  esac
+  [[ ${COMPREPLY[0]} == *= ]] && compopt -o nospace
+}
+complete -F _rl rl
---
+++ fish/rl.fish
@@ -0,0 +1,3 @@
+complete -c rl -s d -l delimiter -d 'delimit line by DELIM'
+complete -c rl -l help -d 'display this help text and exit'
+complete -c rl -l version -d 'output version information and exit'
