4c7d18b7876491bed67fa7e01abbd5eb48ca04a5
kusabashira <kusabashira227@gmail.com>
2016-01-02 14:39:27 +0900

    Update completions for rl

--- fish/rl.fish
+++ fish/rl.fish
@@ -1,3 +1,3 @@
 complete -c rl -s d -l delimiter -d 'delimit line by DELIM'
-complete -c rl -l help -d 'display this help text and exit'
-complete -c rl -l version -d 'output version information and exit'
+complete -c rl -s h -l help -d 'display this help text and exit'
+complete -c rl -s v -l version -d 'output version information and exit'
