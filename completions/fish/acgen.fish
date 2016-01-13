4349d780cc48129a187a1391aecfe0ae8f927594
kusabashira <kusabashira227@gmail.com>
2016-01-13 21:56:54 +0900

    Add completions for acgen

---
+++ completions/fish/acgen.fish
@@ -0,0 +1,3 @@
+complete -c 'acgen' -s 't' -l 'type' -d 'output auto-completion for specified TYPE'
+complete -c 'acgen' -s 'h' -l 'help' -d 'display this help text and exit'
+complete -c 'acgen' -s 'v' -l 'version' -d 'output version information and exit'
